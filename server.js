e6d4608a17f19f7e9cea22d87e51461fd6d52708
vachan-max <vachanshetty23@gmail.com>
2025-06-14 18:08:47 +0530

    version4

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+require('dotenv').config({ path: './.env1' });
 const mongoose = require('mongoose');
 require('./db.js');
 const {authMiddleware}=require('./jwt.js'); // Import authentication middleware
